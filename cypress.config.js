1d8fd8eaada93c1dc5afb05c1dd712fa07bed314
Renato Araújo <renatoassuncaodearaujo@gmail.com>
2022-08-23 12:36:07 -0300

    update cypress config format

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,33 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "cypress_demo",
   reporter: 'junit',
   reporterOptions: {
     mochaFile: 'cypress/results/output.xml',
   },
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
     // baseUrl: 'http://localhost:8080',
-    // supportFile: 'cypress/support/e2e.js'
+    supportFile: 'cypress/support/e2e.js',
+    supportFile: false,
   },
+  component: {
+    supportFile: false,
+  },
+  env: {
+    LAB_URL: "https://custmgmtapp.eng.alticeusa.net"
+  },
+  retries: {
+    runMode: 1,
+    openMode: 3
+  },
+  watchForFileChanges: true,
+  responseTimeout: 10000,
+  screenshotOnRunFailure: true,
+  screenshotsFolder: "cypress/screenshots",
+  trashAssetsBeforeRuns: false,
+  video: true,
+  videoCompression: 32,
+  videosFolder: "cypress/videos",
+  videoUploadOnPasses: true
 })
